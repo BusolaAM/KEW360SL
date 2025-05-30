311ca922119d227bf7aaeaa885bd4c28aec7c54f
oluwajerry <amosjeremiah92@gmail.com>
2025-05-30 23:37:02 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/header/header.component.jsx'
+
 import HomePage from "./pages/Home.jsx";
 import Footer from './components/footer/footer.component';
 import ServicesPage from "./pages/Services";
